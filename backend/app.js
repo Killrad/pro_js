75596c515729aadd8c030b8016e0bf4ea7733e47
Артём <50969854+Killrad@users.noreply.github.com>
2022-02-07 15:12:48 +0300

    fix back

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@
 import mongoose from 'mongoose'
 import express from 'express'
 import Summary from "./Summary.js";
+import cors from 'cors'
 const app = express()
 const URL_DB = 'mongodb+srv://testapi:test1234@cluster0.g6c8y.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const CV = [
@@ -11,6 +12,14 @@ const CV = [
     {id:4, name:'2322525255253'}
 ]
 app.use(express.json());
+app.use(
+    cors({
+        origin: 'http://localhost:8080',
+        // Allow follow-up middleware to override this CORS for options
+        preflightContinue: true,
+    }),
+);
+
 let x = 0;
 
 app.post('/api/cv/add', ( async (req, res) => {
@@ -41,6 +50,7 @@ app.get('/api/cv',
     try{
         let ans = await Summary.find();
         res.status(200).json(ans);
+
     }
     catch (e){
             res.status(500).json(e);
@@ -50,7 +60,7 @@ app.get('/api/cv',
 app.get('/api/cv/:id',
     async (req, res) => {
         try{
-            let ans = await Summary.findBuId(req.params.id);
+            let ans = await Summary.findById(req.params.id);
             if(!req.params.id){
                 req.status(400).json({message:"ID not defined"})
             }
@@ -65,7 +75,7 @@ app.delete('/api/cv/delete/:id', async (req, res) => {
         if(!req.params.id){
             req.status(400).json({message:"ID not defined"})
         }
-        const summary = await Summary.deleteBuIdAndDelete(req.params.id)
+        const summary = await Summary.deleteByIdAndDelete(req.params.id)
         res.status(200).json(summary);
     }
     catch (e){
@@ -84,6 +94,7 @@ async function startApp(){
     try{
         mongoose.connect(URL_DB, {useUnifiedTopology:true, useNewUrlParser: true})
         app.listen(5000,()=> console.log('server has been started on port 5000...'))
+
 
     }
     catch (e){
